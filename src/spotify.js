f4163ccf301bdba1f9e7095ecb6925a40fc984ac
yash kohli <yashkohli786@gmail.com>
2020-08-11 02:56:36 +0530

    user authentication scope and data set using spotify api

---
+++ src/spotify.js
@@ -0,0 +1,17 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "c4f6b365772d4e02ba066c2da4cfd02e";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
