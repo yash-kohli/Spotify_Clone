8b12123ffd4af5bdf256fbcfe74e59722f6bcba9
yash kohli <yashkohli786@gmail.com>
2020-08-12 23:56:52 +0530

    authentication complete and response access token fetched from url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
-
+import { getTokenFromUrl } from "./spotify";
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("token here===>", token);
+  }, []);
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>Logged in bro</h1> : <Login />}</div>
   );
 }
 export default App;
