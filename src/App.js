eccb8592265dfc2192500d6e97355026bfcc263e
yash kohli <yashkohli786@gmail.com>
2020-08-21 02:03:05 +0530

    sidebar completed,data dynamically pulled from spotify api into app

--- src/App.js
+++ src/App.js
@@ -28,6 +28,13 @@ function App() {
           user: user,
         });
       });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
     }
   }, []);
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,32 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
+
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  {
+    /* pulling playlists from dataLayer */
+  }
   return (
     <div className="sidebar">
-      <h1>Side bar here</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="spotify"
+      />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLIST</strong>
+      <hr /> {/* horizontal line gets added */}
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
     </div>
   );
 }
